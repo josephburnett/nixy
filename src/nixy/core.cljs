a0ab8713167946aed7873b1d15130b7eaa1ac816
Joseph Burnett <josephburnett79@gmail.com>
2019-11-25 19:29:12 +0100

    Render stub of nixy, speech bubble and keyboard on 2d canvas.

--- src/nixy/core.cljs
+++ src/nixy/core.cljs
@@ -7,6 +7,11 @@
 (def initial-state
   {:file-system
    {}
+
+   ;; TODO: update this on the 'resize' event.
+   :view
+   {:width 500
+    :height 500}
    
    :character
    {:appearance
@@ -21,12 +26,30 @@
 
 (defonce app-state (atom initial-state))
 
-(defn get-app-element []
-  (gdom/getElement "app"))
+
+; TODO: resize based on width and height
+(defn draw-nixy [ctx state]
+  (aset ctx "lineWidth" 5)
+  (.strokeRect ctx 3 3 50 50))
+
+(defn draw-speech-bubble [ctx state]
+  (aset ctx "lineWidth" 4)
+  (.strokeRect ctx 70 3 120 50))
+
+(defn draw-keyboard [ctx state]
+  (aset ctx "lineWidth" 3)
+  (.strokeRect ctx 3 70 200 100))
+
+(defn render []
+  (let [canvas (gdom/getElement "app")
+        ctx (.getContext canvas "2d")
+        state @app-state]
+    (aset canvas "width" (get-in state [:view :width]))
+    (aset canvas "height" (get-in state [:view :height]))
+    (draw-nixy ctx state)
+    (draw-speech-bubble ctx state)
+    (draw-keyboard ctx state)))
 
 ;; specify reload hook with ^;after-load metadata
 (defn ^:after-load on-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  (render))
