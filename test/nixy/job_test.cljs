6c703c36d99539478f98e1bad311a2bba0dc456e
Joseph Burnett <josephburnett79@gmail.com>
2020-08-22 06:36:44 +0200

    Lean more heavily on dynamic binding in job test.

--- test/nixy/job_test.cljs
+++ test/nixy/job_test.cljs
@@ -5,28 +5,39 @@
    [nixy.fixtures :refer [deep-merge null-cookies null-guide null-fn]]
    [nixy.job :as job]))
 
-(def test-app-state state/initial-state)
-(def ^:dynamic test-setup null-fn)
-(def ^:dynamic test-guide null-guide)
-(def ^:dynamic test-complete? (fn [_] false))
-(def ^:dynamic test-required-cookies null-cookies)
-(def ^:dynamic test-activated-cookies #{})
+(def ^:dynamic test-job-setup null-fn)
+(def ^:dynamic test-job-guide null-guide)
+(def ^:dynamic test-job-complete? (fn [_] false))
+(def ^:dynamic test-job-required-cookies null-cookies)
 
 (defmethod job/definition :test-job [_]
   {:title "Test Job"
    :setup-fn :test-job
    :guide-fn :test-job
    :complete-fn :test-job
-   :required-cookies test-required-cookies})
+   :required-cookies test-job-required-cookies})
 
 (defmethod job/setup :test-job [{:keys [state]}]
-  (test-setup state))
+  (test-job-setup state))
 
 (defmethod job/guide :test-job [{:keys [state line]}]
-  (test-guide state line))
+  (test-job-guide state line))
 
 (defmethod job/complete? :test-job [{:keys [state]}]
-  (test-complete? state))
+  (test-job-complete? state))
+
+(def ^:dynamic test-state-cookies #{})
+(def ^:dynamic test-state-all-jobs #{})
+(def ^:dynamic test-state-active-jobs #{})
+(def ^:dynamic test-state-complete-jobs #{})
+
+(defn test-state []
+  (deep-merge
+   state/initial-state
+   {:cookies test-state-cookies
+    :jobs {:all test-state-all-jobs
+           :active test-state-active-jobs
+           :complete test-state-complete-jobs}}))
 
 (deftest find-new-test
   (let [check (fn [{:keys [required-cookies
@@ -35,28 +46,26 @@
                     :or   {required-cookies #{}
                            have-cookies #{}
                            all-jobs #{}}}]
-                (let [s (deep-merge
-                         test-app-state
-                         {:cookies have-cookies
-                          :jobs {:all all-jobs}})]
-                  (binding [test-required-cookies required-cookies]
-                    (job/find-new s [:test-job]))))]
+                (binding [test-job-required-cookies required-cookies
+                          test-state-cookies        have-cookies
+                          test-state-all-jobs       all-jobs]
+                  (job/find-new (test-state) [:test-job])))]
     (testing "finds new jobs satisfying required cookies"
       (is (= #{:test-job} (check {:required-cookies #{:test-cookie}
-                                  :have-cookies #{:test-cookie}}))    "one cookie")
+                                  :have-cookies #{:test-cookie}}))      "one cookie")
       (is (= #{:test-job} (check {:required-cookies #{:tc-one :tc-two}
-                                  :have-cookies #{:tc-one :tc-two}}))  "two cookies")
+                                  :have-cookies #{:tc-one :tc-two}}))   "two cookies")
       (is (= #{:test-job} (check {:required-cookies #{:tc-one}
-                                  :have-cookies #{:tc-one :tc-two}}))  "extra cookie"))
+                                  :have-cookies #{:tc-one :tc-two}}))   "extra cookie"))
     (testing "does not find jobs already activated"
       (is (= #{} (check {:required-cookies #{:test-cookie}
                          :have-cookies #{:test-cookie}
-                         :all-jobs #{:test-job}}))                     "already activated job"))
+                         :all-jobs #{:test-job}}))                      "already activated job"))
     (testing "does not find jobs not satisfying required cookies"
       (is (= #{} (check {:required-cookies #{:test-cookie}
-                         :have-cookies #{:not-cookie}}))               "wrong cookie")
+                         :have-cookies #{:not-cookie}}))                "wrong cookie")
       (is (= #{} (check {:required-cookies #{:tc-one :tc-two}
-                         :have-cookies #{:tc-one}}))                   "incomplete set of cookies")
+                         :have-cookies #{:tc-one}}))                    "incomplete set of cookies")
       )))
 
 ;; (deftest activate-test
